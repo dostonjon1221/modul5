2fbb1b54b4376c00f94fdae12b505648c5cb7546
dostonjon1221 <dostonjontangirov412@gmail.com>
2023-01-20 17:13:56 -1200

    modul 5 amaliyot js

---
+++ jsonClonlash.js
@@ -0,0 +1,12 @@
+"use strict";
+// const car={
+//     name:"X",
+//     yaer:2002,
+//     extra:{
+//         color:"red"
+//     }
+// };
+// const clone = JSON.parse(JSON.stringify(car));  //clonlash
+// clone.extra.color = "blue"
+// console.log(clone);
+// console.log(car)
